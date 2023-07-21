784ed3ad0519ee7e96db2dcb90230f03be864413
Andrii Chychkan <mywork1728@gmail>
2023-07-22 00:10:54 +0300

    added all partails/css/js/img-folders

--- src/main.js
+++ src/main.js
@@ -1,2 +1,13 @@
 // приклад підключення
 // import './js/swiper';
+
+import './js/header';
+import './js/hero';
+import './js/favorites';
+import './js/search';
+import './js/recipes';
+import './js/popular-recipes';
+import './js/favorites';
+import './js/recipe-modal';
+import './js/rating-modal';
+import './js/order-now-modal';
