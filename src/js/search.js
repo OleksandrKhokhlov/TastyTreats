1467eaf9b3d53c754e6cd1237a7e2566a7715875
erik465 <73741157+erik465@users.noreply.github.com>
2023-07-22 16:06:08 +0200

    Feature/filters search (#7)
    * .
    
    * Update search.html
    
    * .
    
    * Update search.css
    
    * .

---
+++ src/js/search.js
@@ -0,0 +1,38 @@
+import axios from "axios";
+
+const areaSelectElement = document.querySelector('#area-key');
+const ingredientsSelectElement = document.querySelector('#ingredients-key');
+
+const getAreaKeys = async () =>{
+    const response = await axios.get('https://tasty-treats-backend.p.goit.global/api/areas')
+    
+    const result = await response.data
+    return result
+}
+
+const getIngredientsKeys = async () =>{
+    const response = await axios.get('https://tasty-treats-backend.p.goit.global/api/ingredients')
+    
+    const result = await response.data
+    return result
+}
+
+const areaKeys = getAreaKeys()
+.then((results) => {
+    for(let result in results){
+        areaSelectElement.insertAdjacentHTML('beforeend',
+        `
+            <option value="${results[result].name.toLowerCase()}">${results[result].name}</option>
+        `)
+    }
+})
+
+const ingredientsKeys = getIngredientsKeys()
+.then((results) => {
+    for(let result in results){
+        ingredientsSelectElement.insertAdjacentHTML('beforeend',
+        `
+            <option value="${results[result].name.toLowerCase()}">${results[result].name}</option>
+        `)
+    }
+})
