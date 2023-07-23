7959ac22baaae3e24eaece3ef719018308fa39de
erik465 <ehalasz06@gmail.com>
2023-07-23 14:16:08 +0200

    .

--- src/js/search.js
+++ src/js/search.js
@@ -1,9 +1,7 @@
 import axios from "axios"
-
-
+import SlimSelect from 'slim-select'
 import _ from "lodash"
 
-const selectedCategoryEL = document.querySelector('.menu-item .active')
 
 const formEl = document.querySelector('.search-filters');
 
@@ -11,11 +9,7 @@ const searchSelectEl = document.querySelector('#search-key');
 const timeSelectEl = document.querySelector('#time-key')
 const areaSelectElement = document.querySelector('#area-key');
 const ingredientsSelectElement = document.querySelector('#ingredients-key');
-
 const resetFiltersEl = document.querySelector('.filter-reset')
-
-
-
 
 const getAreaKeys = async () =>{
     const response = await axios.get('https://tasty-treats-backend.p.goit.global/api/areas')
@@ -31,7 +25,7 @@ const getIngredientsKeys = async () =>{
     return result
 }
 
-const areaKeys = getAreaKeys()
+getAreaKeys()
 .then((results) => {
     for(let result in results){
         areaSelectElement.insertAdjacentHTML('beforeend',
@@ -41,7 +35,7 @@ const areaKeys = getAreaKeys()
     }
 })
 
-const ingredientsKeys = getIngredientsKeys()
+getIngredientsKeys()
 .then((results) => {
     for(let result in results){
         ingredientsSelectElement.insertAdjacentHTML('beforeend',
@@ -52,15 +46,43 @@ const ingredientsKeys = getIngredientsKeys()
 
 })
 
-const recipesReq = async () =>{
-    const response = await axios.get(
-        `https://tasty-treats-backend.p.goit.global/api/recipes?
-        title=${searchSelectEl.value}&area=${areaSelectElement.value}
-        &time=${timeSelectEl.value}&ingredient=${ingredientsSelectElement.value}
-        &page=1&limit=6`)
-    
-    console.log("RESULT: " + response)
-}
+
+new SlimSelect({
+    select : timeSelectEl,
+    settings : {
+        showSearch : false,
+        placeholderText: '0 min',
+        allowDeselect: true,
+    }
+})
+
+new SlimSelect({
+    select : areaSelectElement,
+    settings : {
+        showSearch : false,
+        placeholderText: 'Region',
+        allowDeselect: true,
+    }
+})
+
+new SlimSelect({
+    select : ingredientsSelectElement,
+    settings : {
+        showSearch : false,
+        placeholderText: 'Product',
+        allowDeselect: true,
+    }
+})
+
+//const recipesReq = async () =>{
+//    const response = await axios.get(
+//        `https://tasty-treats-backend.p.goit.global/api/recipes?
+//        title=${searchSelectEl.value}&area=${areaSelectElement.value}
+//        &time=${timeSelectEl.value}&ingredient=${ingredientsSelectElement.value}
+//        &page=1&limit=6`)
+//    
+//   console.log("RESULT: " + response)
+//}
 
 formEl.addEventListener('change', _.debounce(() => {
     recipesReq()
