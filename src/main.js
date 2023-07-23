0ae1d5e1ce21e24eb694818da1d26fda2f23de1a
Andrii Chychkan <mywork1728@gmail>
2023-07-24 01:14:55 +0300

    fixed

--- src/main.js
+++ src/main.js
@@ -1,6 +1,6 @@
 // приклад підключення
 // import './js/swiper';
-import './js/pagin'
+import './js/pagin';
 import './js/header';
 import './js/hero';
 import './js/favorites';
@@ -11,4 +11,4 @@ import './js/favorites';
 import './js/recipe-modal';
 import './js/rating-modal';
 import './js/categories.js';
-import './js/order-now-modal';
+// import './js/order-now-modal';
