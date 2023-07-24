06a7510408883ded431360ed95f7772e575dc1d6
Misha Flyunt <98420437+MishaFlyunt@users.noreply.github.com>
2023-07-24 16:31:35 +0300

    faorites

---
+++ src/js/favorites/updateCategory.js
@@ -0,0 +1,25 @@
+// import { getFromLocalStorage } from '../favorite/localStorageUtils';
+import refs from './refs';
+
+export function renderCategories(category) {
+  return `<button class="favorites-btn">${category}</button>`;
+}
+
+export function updateCategoryList(target) {
+  const currentRec = target.closest('div.recipe-item').dataset.category;
+  const storageItems = getFromLocalStorage('favorites-data');
+  const isCategoryLocal = storageItems.find(el => el.category === currentRec);
+  const isCategoryRendered = [...refs.categoriesFavorites.children].find(
+    el => el.textContent === currentRec
+  );
+
+  if (!isCategoryLocal && isCategoryRendered) isCategoryRendered.remove();
+  else if (isCategoryLocal && !isCategoryRendered)
+    refs.categoriesFavorites.insertAdjacentHTML(
+      'beforeend',
+      renderCategories(currentRec)
+    );
+
+  refs.allCategoriesBtn.style.display =
+    storageItems && storageItems.length ? 'block' : 'none';
+}
