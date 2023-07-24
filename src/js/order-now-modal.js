f119d80749ec15244c7de6b96f8d43a3fe2a5c46
M0sesz <rodrigescouy25@gmail.com>
2023-07-24 22:00:15 +0300

    123

--- src/js/order-now-modal.js
+++ src/js/order-now-modal.js
@@ -1,66 +1,34 @@
 const refss = {
-  openModalBtns: document.querySelectorAll('.shop-btn'),
+  openModalBtn: document.querySelector('.shop-btn'),
+  //   openModalBtnHero: document.querySelector('.btn-hero'),
   closeModalBtn: document.querySelector('.order-modal-close-btn'),
   backdrop: document.querySelector('.backdrop-order'),
   modal: document.querySelector('.modal-order'),
   forma: document.querySelector('.modal-form-order'),
   input: document.querySelectorAll('.input-js'),
 };
+try {
+  refss.openModalBtnHero.addEventListener('click', openModalOpen);
+} catch (error) {}
 
-refss.openModalBtns.forEach(btn => {
-  btn.addEventListener('click', openModalOpen);
-});
-
+refss.openModalBtn.addEventListener('click', openModalOpen);
 refss.closeModalBtn.addEventListener('click', closeModalClose);
 refss.backdrop.addEventListener('click', clickBackdropClick);
 
-refss.forma.addEventListener('submit', function (e) {
+refss.forma.addEventListener('submit', sendOrder);
+function sendOrder(e) {
   e.preventDefault();
-  if (validateFormAndShowAlert()) {
-    const { name, tel, email, comment } = refss.forma.elements;
-    const result = {
-      name: name.value,
-      tel: tel.value,
-      email: email.value,
-      comment: comment.value,
-    };
-    console.log(result);
+  const { name, tel, email, comment } = e.currentTarget;
+  const result = {
+    name: name.value,
+    tel: tel.value,
+    email: email.value,
+    comment: comment.value,
+  };
 
-    refss.input.forEach(input => {
-      input.value = '';
-      input.classList.remove('valid', 'invalid');
-    });
-
-    closeModalClose();
-  }
-});
-
-function validateInput(input) {
-  if (input.checkValidity()) {
-    input.classList.add('valid');
-    input.classList.remove('invalid');
-  } else {
-    input.classList.remove('valid');
-    input.classList.add('invalid');
-  }
-}
-
-function validateFormAndShowAlert() {
-  const inputFields = document.querySelectorAll('.input-js');
-  let isFormValid = true;
-
-  inputFields.forEach(input => {
-    validateInput(input);
-    if (!input.checkValidity()) {
-      isFormValid = false;
-    }
-  });
-
-  if (!isFormValid) {
-    alert('Please fill in all the fields correctly.');
-  }
-
-  return isFormValid;
+  console.log(result);
+  e.currentTarget.reset();
+  closeModalClose();
 }
 
 function openModalOpen() {
@@ -73,6 +41,7 @@ function openModalOpen() {
 function closeModalClose() {
   document.body.classList.remove('overflowHidden');
   window.removeEventListener('keydown', onEscPress);
+  document.body.classList.remove('overflowHidden');
   refss.backdrop.classList.remove('active');
   refss.modal.classList.remove('active');
 }
