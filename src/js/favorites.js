67f0569366827957440a4ff24974f519e39d2a15
Misha Flyunt <98420437+MishaFlyunt@users.noreply.github.com>
2023-07-26 13:45:15 +0300

    favorites

--- src/js/favorites.js
+++ src/js/favorites.js
@@ -1,1 +1,45 @@
-import './favorites/main-favorites';
+import { pagination } from './pagin';
+import renderCards from './render-recipes-cards';
+
+const listFavorites = document.querySelector('.favorites-list');
+const favoritesMessError = document.querySelector('.favorites-error');
+const categoriesFavorites = document.querySelector('.favorites-categories');
+const heroFavorites = document.querySelector('.favorites-hero');
+
+//localStorage
+const recipesFromLocalStorage = JSON.parse(localStorage.getItem('favorites'));
+// console.log(recipesFromLocalStorage);
+// let favRecipesLocalStorage = recipesFromLocalStorage;
+
+function renderCardsFavorites() {
+  listFavorites.insertAdjacentHTML(
+    'beforeend',
+    renderCards(recipesFromLocalStorage)
+  );
+
+  if (recipesFromLocalStorage.length === 0) {
+    favoritesMessError.classList.remove('visually-hidden');
+    categoriesFavorites.classList.add('visually-hidden');
+    if (window.innerWidth < 767) heroFavorites.classList.add('visually-hidden');
+    return;
+  }
+}
+renderCardsFavorites();
+
+//категорії з localStorage
+function createAllCategireisList() {
+  const AllCategireisListItem = recipesFromLocalStorage
+    .map(recipe => {
+      return `<button class="favorites-btn btn-categori is-active"
+                  type="button">${recipe.category}
+                </button>
+                `;
+    })
+    .join('');
+
+  categoriesFavorites.insertAdjacentHTML('beforeend', AllCategireisListItem);
+}
+createAllCategireisList();
+
+//сортування по категоріях
+
